e61bdeb9bc52c46a3ad5a55de8cf85454af409bf
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-27 21:33:55 -0300

    mudanças de cor e posicionamento no header e no botão que mostra a lista

--- Documents/cat-produtos/cat-prod/src/App.js
+++ Documents/cat-produtos/cat-prod/src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Catálogo</h1>
       </header>
-      <nav id="navbar">Barra de navegação</nav>
+      <nav id="navbar" className={isNavExpanded ? 'expanded' : ''}>
+        <button className="nav-toggle" onClick={toggleNav}>
+          Menu
+        </button>
+        <ul id="nav-items" className={!isNavExpanded ? 'nav-closed' : ''}>
+          <li className="items">item 1</li>
+          <li className="items">item 2</li>
+          <li className="items">item 3</li>
+        </ul>
+      </nav>
     </div>
   );
 }
