76118c5732573522f93f4d09536acb553cecf69d
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-30 20:59:18 -0300

    adição do product details do conjunto intimo

--- Documents/cat-produtos/cat-prod/src/components/ProductDetail.js
+++ Documents/cat-produtos/cat-prod/src/components/ProductDetail.js
@@ -13,7 +13,21 @@ const ProductDetail = () => {
       image: 'https://th.bing.com/th/id/R.a39607287fff38e155bbd2143ce55e6b?rik=KJTSQY6IxhACvg&pid=ImgRaw&r=0',
       description: 'Este é um sutiã confortável e elegante, perfeito para o dia a dia.',
     },
-    { id: 2, name: 'Calcinha', price: 19.90, category: 'lingeries', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_971684-MLB48848823458_012022-F.webp', description: 'Esta é uma calcinha confortável e elegante, perfeito para o dia a dia.' },
+    { id: 2, 
+      name: 'Calcinha', 
+      price: 19.90, 
+      category: 'lingeries', 
+      image: 'https://http2.mlstatic.com/D_NQ_NP_2X_971684-MLB48848823458_012022-F.webp', 
+      description: 'Esta é uma calcinha confortável e elegante, perfeito para o dia a dia.' },
+
+      {
+        id: 3,
+        name: 'Conjunto íntimo',
+        price: 59.90, 
+        category: 'conjuntos', 
+        image: 'https://http2.mlstatic.com/D_NQ_NP_2X_766616-MLB47177416741_082021-F.jpg',
+        description: "Um conjunto para a noite",
+      },
   ];
 
   const product = products.find(product => product.id === parseInt(id));
